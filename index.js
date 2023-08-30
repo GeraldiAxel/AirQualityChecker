c759ffe64f83f78c37213e5bfefc57aa431d085c
GeraldiAxel <geraldi.axel@gmail.com>
2023-08-30 18:17:25 +0700

    move map to main page and remove header

--- index.js
+++ index.js
@@ -9,11 +9,27 @@ const API_TOKEN = "fb53451ee89052f1ce893b7f8e408a0cdde9ff7b";
 
 const BASE_URL = "https://api.waqi.info";
 
+async function fetchDefaultCityData() {
+    try {
+        const response = await axios.get(BASE_URL + `/feed/jakarta/?token=${API_TOKEN}`);
+        return {
+            cityName: response.data.data.city.name,
+            airQuality: response.data.data.aqi,
+            weekly: response.data.data.forecast.daily.pm10,
+        };
+    } catch (error) {
+        return {
+            errorMessage: `The city "Jakarta" data is not available. Please try a different city.`,
+        };
+    }
+}
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+    const defaultCityData = await fetchDefaultCityData();
+    res.render("index.ejs", defaultCityData);
 });
 
 app.get("/map", (req, res) => {
@@ -23,7 +39,7 @@ app.get("/map", (req, res) => {
 app.post("/", async (req, res) => {
     try{
         const response = await axios.get(BASE_URL + `/feed/${req.body.city}/?token=${API_TOKEN}`);
-        console.log(response.data.data.forecast.daily.pm10);
+        console.log(response.data.data.forecast);
         res.render("index.ejs", {
             cityName: response.data.data.city.name,
             airQuality: response.data.data.aqi,
